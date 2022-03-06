3aab293961b0828c3235882073aa804f0ea1a200
DovalehB <70105106+DovalehB@users.noreply.github.com>
2022-03-06 14:59:53 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Dov Barak Home Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
